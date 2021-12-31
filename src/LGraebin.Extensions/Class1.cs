b5e15f90bfd0d7bc71da9ddfa7bbcc370a42e0c8
Lucas Graebin <lgraebin@gmail.com>
2021-12-31 09:00:23 -0300

    feat: add empty classlib and xunit projects

---
+++ src/LGraebin.Extensions/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LGraebin.Extensions
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/LGraebin.Extensions.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace LGraebin.Extensions.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
