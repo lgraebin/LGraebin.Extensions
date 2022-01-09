5d7723d70234556b7ab1cc1c263579b681abf3cd
Lucas Graebin <lgraebin@gmail.com>
2022-01-09 17:01:22 -0300

    feat: add extension method to mask local-part of e-mail address

---
+++ src/LGraebin.Extensions/StringManipulation/MaskEmail.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LGraebin.Extensions.StringManipulation
+{
+    public static partial class StringManipulationExtensions
+    {
+        private static readonly Regex MaskRegex = new Regex(@"(?<=[^@]{1})[^@]*(?=[^@]{1}@)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Mask the local-part (the bit before the @-sign) of e-mail address by replacing all characters with *, except the first and last characters.
+        /// </summary>
+        /// <param name="email">E-mail to mask.</param>
+        /// <returns>The e-mail address with the local-part masked.</returns>
+        public static string MaskEmail(this string email)
+        {
+            return MaskRegex.Replace(email, x => new string('*', x.Length));
+        }
+    }
+}
---
+++ tests/LGraebin.Extensions.UnitTests/StringManipulation/MaskEmailTests.cs
@@ -0,0 +1,28 @@
+﻿using LGraebin.Extensions.StringManipulation;
+using Xunit;
+
+namespace LGraebin.Extensions.UnitTests.StringManipulation
+{
+    public class MaskEmailTests
+    {
+        [Theory]
+        [InlineData("valid@example.com", "v***d@example.com")]
+        [InlineData("valid+valid123@test.example.com", "v************3@test.example.com")]
+        [InlineData("valid_valid123@test.example.com", "v************3@test.example.com")]
+        [InlineData("valid-valid+123@test.example.co.uk", "v*************3@test.example.co.uk")]
+        [InlineData("valid-valid+1.23@test.example.com.au", "v**************3@test.example.com.au")]
+        [InlineData("valid_@example.com", "v****_@example.com")]
+        [InlineData("valid123.456@example.org", "v**********6@example.org")]
+        [InlineData("valid-@example.com", "v****-@example.com")]
+        [InlineData("customer/department=shipping@example.com", "c**************************g@example.com")]
+        [InlineData("$A12345@example.com", "$*****5@example.com")]
+        [InlineData("!def'`xyz%abc@example.com", "!***********c@example.com")]
+        [InlineData("_somename@example.com", "_*******e@example.com")]
+        public void ShouldMaskEmailAddress(string email, string expected)
+        {
+            var result = email.MaskEmail();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
