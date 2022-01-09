ae0b2cfe3a2fed9e94ae70937e744fe4d113901d
Lucas Graebin <lgraebin@gmail.com>
2022-01-09 16:56:49 -0300

    docs: update documentation of IsEmail method

--- src/LGraebin.Extensions/Verification/IsEmail.cs
+++ src/LGraebin.Extensions/Verification/IsEmail.cs
@@ -7,10 +7,10 @@ namespace LGraebin.Extensions.Verification
         private static readonly Regex EmailRegex = new Regex(@"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         /// <summary>
-        /// Check if a given string is a valid e-mail address.
+        /// Check if the string is a valid e-mail address.
         /// </summary>
         /// <param name="email">String to check.</param>
-        /// <returns>TRUE if the given string is a valid e-mail address; otherwise, FALSE.</returns>
+        /// <returns>True if the string is a valid e-mail address; otherwise, false.</returns>
         public static bool IsEmail(this string email)
         {
             return EmailRegex.IsMatch(email);
